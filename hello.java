edf6a24272ed8537179799df4be646eae8b1e943
CinaDor <cinador@github.com>
2024-11-08 13:51:50 +0900

    add: addTest

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class hello {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+
+        System.out.print(list.toString());
+    }
+}
